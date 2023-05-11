87658022b3ecf6eb5d021ac486324ec95c0a23bb
abhishekmalviya <abhishek@pillow.fund>
2023-05-11 11:59:29 +0530

    interactive prompt

--- build/index.js
+++ build/index.js
@@ -6,31 +6,53 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.YargsError = exports.getCli = exports.yarg = void 0;
 const yargs_1 = __importDefault(require("yargs"));
 const helpers_1 = require("yargs/helpers");
+const yargs_interactive_1 = __importDefault(require("yargs-interactive"));
 exports.yarg = (0, yargs_1.default)(helpers_1.hideBin(process.argv));
 const topBanner = `Abhishek talks: is an interactive commandline cli for chatGPT`;
 const bottomBanner = `feel free to call me lord`;
 function getCli() {
     const cli = exports.yarg
         .usage(topBanner)
-        .epilogue(bottomBanner);
+        .epilogue(bottomBanner).scriptName("").demandCommand(1);
     return cli;
 }
 exports.getCli = getCli;
 class YargsError extends Error {
 }
 exports.YargsError = YargsError;
-const cli = getCli();
-cli.fail((msg, err) => {
-    if (msg) {
-        // Show command help message when no command is provided
-        if (msg.includes("Not enough non-option arguments")) {
-            exports.yarg.showHelp();
-            // eslint-disable-next-line no-console
-            console.log("\n");
-        }
+// const cli = getCli()
+// cli.fail((msg, err) => {
+//     if (msg) {
+//         // Show command help message when no command is provided
+//         if (msg.includes("Not enough non-option arguments")) {
+//             yarg.showHelp()
+//             // eslint-disable-next-line no-console
+//             console.log("\n")
+//         }
+//     }
+//     const errorMessage =
+//         err !== undefined ? (err instanceof YargsError ? err.message : err.stack) : msg || "Unknown error"
+//     // eslint-disable-next-line no-console
+//     console.error(` ✖ ${errorMessage}\n`)
+//     process.exit(1)
+// }).parse()
+// ####################
+const options = {
+    interactive: { default: true },
+    prompt: {
+        type: "input",
+        describe: "Enter your prompt"
     }
-    const errorMessage = err !== undefined ? (err instanceof YargsError ? err.message : err.stack) : msg || "Unknown error";
-    // eslint-disable-next-line no-console
-    console.error(` ✖ ${errorMessage}\n`);
-    process.exit(1);
-}).parse();
+};
+function help() {
+    (0, yargs_interactive_1.default)()
+        .usage("$0 <command> [args]")
+        .interactive(options)
+        .then(result => {
+        // The tool will prompt questions and will output your answers.
+        // TODO: Do something with the result (e.g result.name)
+        console.log(result.prompt);
+        help();
+    });
+}
+help();
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,22 @@
-import yargs from 'yargs'
-import {hideBin} from 'yargs/helpers'
+import yargsInteractive from 'yargs-interactive';
 
-export const yarg = yargs((hideBin as (args: string[]) => string[])(process.argv))
 
-const topBanner  = `Abhishek talks: is an interactive commandline cli for chatGPT`
-const bottomBanner = `feel free to call me lord`
-
-export function getCli():yargs.Argv{
-    const cli  = yarg
-    .usage(topBanner)
-        .epilogue(bottomBanner)
-
-        return cli;
-}
-export class YargsError extends Error {}
-
-const cli =getCli()
-
-cli.fail((msg, err) => {
-    if (msg) {
-        // Show command help message when no command is provided
-        if (msg.includes("Not enough non-option arguments")) {
-            yarg.showHelp()
-            // eslint-disable-next-line no-console
-            console.log("\n")
-        }
+const options: any = {
+    interactive: { default: true },
+    prompt: {
+        type: "input",
+        describe: "Enter your prompt"
     }
+};
 
-    const errorMessage =
-        err !== undefined ? (err instanceof YargsError ? err.message : err.stack) : msg || "Unknown error"
+function prompt() {
+    yargsInteractive()
+        .usage("$0 <command> [args]")
+        .interactive(options)
+        .then(result => {
+            console.log(result.prompt);
+            prompt();
+        });
+}
 
-    // eslint-disable-next-line no-console
-    console.error(` ✖ ${errorMessage}\n`)
-    process.exit(1)
-}).parse()
+prompt()
